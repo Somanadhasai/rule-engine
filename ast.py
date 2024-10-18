b47101a7a68157131a6940d053970a0600ff2f10
somanadha sai <149456872+Somanadhasai@users.noreply.github.com>
2024-10-18 20:34:10 +0530

    Delete ast.py

--- ast.py
+++
@@ -1,6 +0,0 @@
-class Node:
-    def __init__(self, node_type, left=None, right=None, value=None):
-        self.node_type = node_type  # "operator" or "operand"
-        self.left = left            # Reference to left child Node
-        self.right = right          # Reference to right child Node
-        self.value = value          # Optional value for operand nodes
